428c4c22aeb0130f925706f858fe69499ad33ec3
cookiehaven <holmzjack@gmail.com>
2025-07-27 09:56:22 +0330

    Update products.js

--- products.js
+++ products.js
@@ -1,5 +1,5 @@
 const products = [
-  { id: 1, name: "کوکی شکلاتی", category: "cookie", price: 50000, image: "images/cookie-chocolate.jpg" },
+  { id: 1, name: "کوکی شکلاتی", category: "cookie", price: 80000, image: "images/cookie-chocolate.jpg" },
   { id: 2, name: "چیزکیک لوتوس", category: "cheesecake", price: 80000, image: "images/lotos.chesese.cake.jpg" },
   { id: 3, name: "کوکی بادام‌زمینی", category: "cookie", price: 55000, image: "images/cookie-peanut.jpg" }
 ];
