19aa667ba878678cfbab126de32777180d7cd93d
cookiehaven <holmzjack@gmail.com>
2025-07-27 11:02:00 +0330

    Update products.js

--- products.js
+++ products.js
@@ -1,12 +1,12 @@
 const products = [
   { id: 1, name: "کوکی شکلاتی", category: "cookie", price: 50000, image: "images/cookie-chocolate.jpg" },
-  { id: 2, name: "چیزکیک لوتوس", category: "cheesecake", price: 50000, image: "images/lotos.chesese.cake.jpg" },
+  { id: 2, name: "چیزکیک لوتوس", category: "cheesecake", price: 1100000, image: "images/lotos.chesese.cake.jpg" },
   { id: 3, name: "کوکی بادام‌زمینی", category: "cookie", price: 55000, image: "images/cookie-peanut.jpg" },
-  { id: 4, name: "چیزکیک مارس", category: "cheesecake", price: 80000, image: "images/cheese.cake.mars.jpg" },
-  { id: 5, name: "کیک هویج گردو", category: "cake", price: 50000, image: "images/cake.havij.gerdo.jpg" },
-  { id: 6, name: "کیک خیس شکلاتی", category: "cake", price: 50000, image: "images/cake.khis.shokolati.jpg" },
-  { id: 7, name: "کیک ردولوت", category: "cake", price: 50000, image: "images/cake.redvevet.jpg" },
-  { id: 8, name: "کیک نوتلا", category: "cake", price: 50000, image: "images/cheeese.cake.nutela.jpg" },
+  { id: 4, name: "چیزکیک مارس", category: "cheesecake", price: 1300000, image: "images/cheese.cake.mars.jpg" },
+  { id: 5, name: "کیک هویج گردو", category: "cake", price: 500000, image: "images/cake.havij.gerdo.jpg" },
+  { id: 6, name: "کیک خیس شکلاتی", category: "cake", price: 750000, image: "images/cake.khis.shokolati.jpg" },
+  { id: 7, name: "کیک ردولوت", category: "cake", price: 800000, image: "images/cake.redvevet.jpg" },
+  { id: 8, name: "چیزکیک نوتلا", category: "cake", price: 1000000, image: "images/cheeese.cake.nutela.jpg" },
 ];
 
 function renderProducts(productArray) {
