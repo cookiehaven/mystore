3948d598c4830b098bcc818057715f99170a0652
cookiehaven <holmzjack@gmail.com>
2025-07-27 10:27:06 +0330

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const products = [
-  { id: 1, name: "کوکی شکلاتی", category: "cookie", price: 80000, image: "images/cookie-chocolate.jpg" },
-  { id: 2, name: "چیزکیک لوتوس", category: "cheesecake", price: 80000, image: "images/lotos.chesese.cake.jpg" },
+  { id: 1, name: "کوکی شکلاتی", category: "cookie", price: 50000, image: "images/cookie-chocolate.jpg" },
+  { id: 2, name: "چیزکیک لوتوس", category: "cheesecake", price: 50000, image: "images/lotos.chesese.cake.jpg" },
   { id: 3, name: "کوکی بادام‌زمینی", category: "cookie", price: 55000, image: "images/cookie-peanut.jpg" },
   { id: 4, name: "چیزکیک مارس", category: "cheesecake", price: 80000, image: "images/cheese.cake.mars.jpg" },
   { id: 5, name: "کیک هویج گردو", category: "cake", price: 50000, image: "images/cake.havij.gerdo.jpg" },
-  { id: 6, name: "کیک خیس شکلاتی", category: "cake", price: 80000, image: "images/cake.khis.shokolati.jpg" },
+  { id: 6, name: "کیک خیس شکلاتی", category: "cake", price: 50000, image: "images/cake.khis.shokolati.jpg" },
 ];
 
 function renderProducts(productArray) {
