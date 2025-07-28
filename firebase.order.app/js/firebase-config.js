dc809984b446366c2f64a8252c999533ec061dfa
cookiehaven <holmzjack@gmail.com>
2025-07-28 14:15:33 +0330

    Update firebase-config.js

--- firebase.order.app/js/firebase-config.js
+++ firebase.order.app/js/firebase-config.js
@@ -3,6 +3,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyDbJwM4NRLxxgRxIPDpzV6T1wDaBjVmMtw",
   authDomain: "mycookie-a1439.firebaseapp.com",
   projectId: "mycookie-a1439",
+  storageBucket: "mycookie-a1439.firebasestorage.app",
+  messagingSenderId: "488534717537",
+  appId: "1:488534717537:web:1243d05aac7dca90802620"
 };
 
 firebase.initializeApp(firebaseConfig);
